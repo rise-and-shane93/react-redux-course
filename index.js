f4a6889978ac658a55d4cd11b4a97ee4c47e2a0f
Shane Harper <sharper@mosaiclearning.com>
2019-07-25 16:22:59 -0400

    start redux implementation

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Scoreboard from './scoreboard';
+import Scoreboard from './src/containers/Scoreboard';
 
 render(
     <Scoreboard />,
---
+++ src/containers/Scoreboard.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import AddPlayerForm from '../components/AddPlayerForm';
+import Player from '../components/Player';
+import Header from '../components/Header';
+
+export default class Scoreboard extends Component {
+
+  onScoreChange = (index, delta) => {
+    this.state.players[index].score += delta;
+    this.setState(this.state);
+  };
+
+  onAddPlayer = (name) => {
+    this.state.players.push({ name: name, score: 0 });
+    this.setState(this.state);
+  };
+
+  onRemovePlayer = (index) => {
+    this.state.players.splice(index, 1);
+    this.setState(this.state);
+  };
+
+  render() {
+    return (
+      <div className="scoreboard">
+        <Header players={this.state.players} />
+        <div className="players">
+          {this.state.players.map(function(player, index) {
+             return (
+               <Player
+                 name={player.name}
+                 score={player.score}
+                 key={player.name}
+                 onScoreChange={(delta) => this.onScoreChange(index, delta)}
+                 onRemove={() => this.onRemovePlayer(index)}
+               />
+             );
+           }.bind(this))}
+        </div>
+        <AddPlayerForm onAdd={this.onAddPlayer} />
+      </div>
+    );
+  }
+};
